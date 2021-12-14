7b70b002112e4b4d5b4c0e0fcbc3f953cf78744c
agiltandiera <agiltandiera27@gmail.com>
2021-12-14 13:39:45 +0800

    develop splashScreen

--- app/src/main/java/com/tandiera/project/movbooking/SplashScreenActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.tandiera.project.movbooking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import com.tandiera.project.movbooking.onBoarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity,
+                OnBoardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 }
