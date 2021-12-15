3f01dcf20daac17e8619faec920f72057de4cb2c
agiltandiera <agiltandiera27@gmail.com>
2021-12-15 10:28:05 +0800

    modify onBoardingOne

--- app/src/main/java/com/tandiera/project/movbooking/SignInActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/SignInActivity.kt
@@ -4,26 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.tandiera.project.movbooking.onBoarding.OnBoardingTwoActivity
 import com.google.firebase.database.DatabaseReference
 
 import com.google.firebase.database.FirebaseDatabase
 
 
-
-
 class SignInActivity : AppCompatActivity() {
+
+    lateinit var  iUsername: String
+    lateinit var  iPassword: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         val button1: Button = findViewById(R.id.btn_pink)
         button1.setOnClickListener {
-            // Write a message to the database
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("message")
-
-            myRef.setValue("Hello, World!")
+            iUsername: EditText = findViewById(R.id.editTxtPass)
+            iUsername = editTxtPass.text.toString()
         }
+
+        val edit: EditText
     }
 }
--- app/src/main/java/com/tandiera/project/movbooking/onBoarding/OnBoardingOneActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/onBoarding/OnBoardingOneActivity.kt
@@ -6,11 +6,25 @@ import android.os.Bundle
 import android.widget.Button
 import com.tandiera.project.movbooking.R
 import com.tandiera.project.movbooking.SignInActivity
+import java.util.prefs.Preferences
 
 class OnBoardingOneActivity : AppCompatActivity() {
+
+//    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_one)
+
+//        preferences = Preferences(this)
+//
+//        if (preferences.getValues("onboarding").equals("1")) {
+//            finishAffinity()
+//
+//            val intent = Intent(this@OnBoardingOneActivity,
+//                SignInActivity::class.java)
+//            startActivity(intent)
+//        }
 
         val button1: Button = findViewById(R.id.btn_pink)
         button1.setOnClickListener {
@@ -20,7 +34,9 @@ class OnBoardingOneActivity : AppCompatActivity() {
 
         val button2: Button = findViewById(R.id.btn_blue)
         button2.setOnClickListener {
+//            preferences.set("onboarding", "1")
             finishAffinity() //hapus semua page yang sudah ditampilkan
+
             val intent = Intent(this@OnBoardingOneActivity, SignInActivity::class.java)
             startActivity(intent)
         }
