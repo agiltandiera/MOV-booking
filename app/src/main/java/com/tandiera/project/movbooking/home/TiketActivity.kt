a97da072440639e020da3b05ef0302c7fe030411
agiltandiera <agiltandiera27@gmail.com>
2021-12-15 11:50:26 +0800

    handle error

--- app/src/main/java/com/tandiera/project/movbooking/home/TiketActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/home/TiketActivity.kt
@@ -2,6 +2,7 @@ package com.tandiera.project.movbooking.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tandiera.project.movbooking.R
 
 class TiketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
