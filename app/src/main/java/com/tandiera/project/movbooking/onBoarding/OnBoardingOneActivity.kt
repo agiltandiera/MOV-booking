99e7e958bea930a2863e0afa2bd680b3defaf83d
agiltandiera <agiltandiera27@gmail.com>
2021-12-18 18:45:31 +0800

    make intent onboarding

--- app/src/main/java/com/tandiera/project/movbooking/onBoarding/OnBoardingOneActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/onBoarding/OnBoardingOneActivity.kt
@@ -10,22 +10,11 @@ import com.tandiera.project.movbooking.utils.Preferences
 
 class OnBoardingOneActivity : AppCompatActivity() {
 
-    lateinit var preferences: Preferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_one)
 
-        preferences = Preferences(this)
-
-        if (preferences.getValues("onboarding").equals("1")) {
-            finishAffinity()
-
-            val intent = Intent(this@OnBoardingOneActivity,
-                SignInActivity::class.java)
-            startActivity(intent)
-        }
-
+//        BUAT LISTENER
         val button1: Button = findViewById(R.id.btn_pink)
         button1.setOnClickListener {
             val intent = Intent(this@OnBoardingOneActivity, OnBoardingTwoActivity::class.java)
@@ -34,9 +23,7 @@ class OnBoardingOneActivity : AppCompatActivity() {
 
         val button2: Button = findViewById(R.id.btn_blue)
         button2.setOnClickListener {
-            preferences.setValues("onboarding", "1")
-            finishAffinity() //hapus semua page yang sudah ditampilkan
-
+            finishAffinity()
             val intent = Intent(this@OnBoardingOneActivity, SignInActivity::class.java)
             startActivity(intent)
         }
