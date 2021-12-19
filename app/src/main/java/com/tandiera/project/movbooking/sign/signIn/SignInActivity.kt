3e22a9dfa5f9ca345dc1390ea32db8de98de3a65
agiltandiera <agiltandiera27@gmail.com>
2021-12-19 20:35:20 +0800

    handle error

--- app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.parcel.Parcelize
 import android.content.Intent
+import android.view.View.inflate
 import android.widget.Toast
 import android.widget.Button
 import android.widget.EditText
@@ -18,19 +19,27 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.tandiera.project.movbooking.databinding.ActivityCheckoutBinding.inflate
+import com.tandiera.project.movbooking.databinding.ActivityCheckoutSuccessBinding.inflate
 import com.tandiera.project.movbooking.onBoarding.OnBoardingTwoActivity
 
 class SignInActivity : AppCompatActivity() {
 
-    lateinit var iUsername : String
-    lateinit var iPassword : String
+//   Initialize Binding
+    private lateinit var binding : SignInActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = SignInActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val button1: Button = findViewById(R.id.btn_blue)
-        btn_blue.setOnClickListener {
+////        val button1: Button = findViewById(R.id.btn_blue)
+//        btn_blue.setOnClickListener {
+//
+//        }
+
+        binding.name.text ="tes"
+        binding.button.setOnClickListener {
 
         }
     }
