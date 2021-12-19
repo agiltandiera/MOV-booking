4e6d22091d5d03ccf40977e234c67f566ee35b62
agiltandiera <agiltandiera27@gmail.com>
2021-12-19 19:37:44 +0800

    sync gradle

--- app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.tandiera.project.movbooking.sign.signIn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.parcel.Parcelize
 import android.content.Intent
 import android.widget.Toast
 import android.widget.Button
@@ -21,17 +22,16 @@ import com.tandiera.project.movbooking.onBoarding.OnBoardingTwoActivity
 
 class SignInActivity : AppCompatActivity() {
 
+    lateinit var iUsername : String
+    lateinit var iPassword : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val button1: Button = findViewById(R.id.btn_pink)
-        button1.setOnClickListener {
-            // Write a message to the database
-            val database = Firebase.database
-            val myRef = database.getReference("message")
+//        val button1: Button = findViewById(R.id.btn_blue)
+        btn_blue.setOnClickListener {
 
-            myRef.setValue("Hello, World!")
         }
     }
 }
