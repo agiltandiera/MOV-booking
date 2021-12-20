5b7c50c998edd48a8aff9a4901044478660d1897
agiltandiera <agiltandiera27@gmail.com>
2021-12-20 13:48:49 +0800

    create preferences file

--- app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/sign/signIn/SignInActivity.kt
@@ -2,28 +2,16 @@ package com.tandiera.project.movbooking.sign.signIn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.parcel.Parcelize
 import android.content.Intent
-import android.view.View.inflate
 import android.widget.Toast
-import android.widget.Button
-import android.widget.EditText
 import com.google.firebase.database.*
 
-import com.tandiera.project.movbooking.R
 import com.tandiera.project.movbooking.home.HomeActivity
 import com.tandiera.project.movbooking.sign.signUp.SignUpActivity
-import com.tandiera.project.movbooking.utils.Preferences
 import com.google.firebase.database.DatabaseReference
 
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.tandiera.project.movbooking.databinding.ActivityCheckoutBinding.bind
-import com.tandiera.project.movbooking.databinding.ActivityCheckoutBinding.inflate
-import com.tandiera.project.movbooking.databinding.ActivityCheckoutSuccessBinding.inflate
 import com.tandiera.project.movbooking.databinding.ActivitySignInBinding
-import com.tandiera.project.movbooking.onBoarding.OnBoardingTwoActivity
 
 class SignInActivity : AppCompatActivity() {
 
