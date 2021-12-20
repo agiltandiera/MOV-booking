6330f42f74e2958964e0a8faff269b128b394e14
agiltandiera <agiltandiera27@gmail.com>
2021-12-20 13:54:43 +0800

    make set and get value in preferences

--- app/src/main/java/com/tandiera/project/movbooking/utils/Preferences.kt
+++ app/src/main/java/com/tandiera/project/movbooking/utils/Preferences.kt
@@ -1,2 +1,23 @@
 package com.tandiera.project.movbooking.utils
 
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preferences (val context: Context) {
+    companion object {
+        const val USER_PREFF = "USER_PREFF"
+    }
+
+    var sharedPreferences = context.getSharedPreferences(USER_PREFF,0)
+
+    fun setValue(key: String, value: String) {
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+//    func untuk ambil value
+    fun getValues(key: String) : String? {
+        return sharedPreferences.getString(key, "")
+    }
+}
