16bc8877f0da57732c69e71a5838fc2cc4da975b
agiltandiera <agiltandiera27@gmail.com>
2022-01-10 14:03:27 +0800

    create layout and adapter for checkout

--- app/src/main/java/com/tandiera/project/movbooking/checkout/CheckoutSuccessActivity.kt
+++ app/src/main/java/com/tandiera/project/movbooking/checkout/CheckoutSuccessActivity.kt
@@ -8,5 +8,7 @@ class CheckoutSuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout_success)
+
+        btn
     }
 }
